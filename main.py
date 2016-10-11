5b4c71c2b016637c246d558bde2fee765e3593a2
adityajain21 <96adityajain@gmail.com>
2016-10-11 17:17:43 +0530

    Added api call for starring all repos

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-import random
-import time
-from uuid import uuid4
-
+import os, requests, json, getpass
 from mechanize import Browser
 
 br = Browser()
@@ -10,13 +6,13 @@ br = Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(False)
 
-
 br.open('https://github.com/login')
 
 br.form=list(br.forms())[0]
 
 _username = str(raw_input("Username for 'https://github.com': "))
-_password = str(raw_input("Password for 'https://" + _username + "@github.com': "))
+_password = str(getpass.getpass("Password for 'https://" + _username + "@github.com': "))
+
 
 email_control = br.form.find_control("login")
 if email_control.type == "text":
@@ -35,3 +31,18 @@ if(br.submit().read().find('Sign in to GitHub')==-1):
 	print("Logged in Succesfully.")
 else:
 	print ("Invalid Credentials. Please try again.")
+	exit()
+
+
+_friendusername = str(raw_input("Username of friend for which you want to star all the repos: "))
+
+url = "https://api.github.com/users/" + _friendusername + "/repos"
+response = requests.get(url)
+data = json.loads(response.text)
+
+for i in data:
+	repo = i['name']
+	url = "https://api.github.com/user/starred/" + _friendusername + "/" + repo
+	requests.delete(url,auth=(_username,_password))
+
+print "DONE!"
